53128f9fc655b6f41c2124753a323c9fa707eb9f
DESKTOP-2G50SHI\user1 <corey5361@nate.com>
2023-10-05 17:39:31 +0900

    23.10.05 오라클 그룹함수 집계함수 집합 연산자 JOIN 작성자 :  양덕기

---
+++ 06_JOIN.sql
@@ -0,0 +1,43 @@
+/*
+    <JOIN>
+        두 개의 이상의 테이블에서 데이터를 조회하고자 할 때 사용하는 구문이다.
+        
+        1. 등가 조인(EQUAL JOIN) / 내부 조인(INNER JOIN)
+            연결시키는 칼럼의 값이 일치하는 행들만 조인되서 조회한다.(일치하는 값이 없는 행은 조회 X)
+            
+            1) 오라클 전용 구문
+                [표현법]
+                    SELECT 칼럼, 칼럼, ...
+                    FROM 테이블1, 테이블2
+                    WHERE 테이블1.칼럼명 = 테이블2.칼럼명;
+                
+                - FROM 절에 조회하고자 하는 테이블들을 콤마(,)로 구분하여 나열한다.
+                - WHERE 절에 매칭 시킬 칼럼명에 대한 조건을 제시한다.
+            
+            2) ANSI 표준 구문
+                [표현법]
+                    SELECT 칼럼, 칼럼, ...
+                    FROM 테이블1
+                    [INNER] JOIN 테이블2 ON (테이블1.칼럼명 = 테이블2.칼럼명);
+                
+                - FROM 절에 기준이 되는 테이블을 기술한다.
+                - JOIN 절에 같이 조회하고자 하는 테이블을 기술 후 매칭 시킬 칼럼에 대한 조건을 기술한다.
+                - 연결에 사용하려는 칼럼명이 같은 경우 ON 구문 대신에 USING(칼럼명) 구문을 사용한다.
+*/
+
+-- 사원명, 부서코드,부서명 조회
+SELECT EMP_NAME,DEPT_CODE, DEPT_TITLE
+FROM EMPLOYEE JOIN DEPARTMENT ON DEPT_CODE = DEPT_ID;
+
+SELECT EMP_NAME,DEPT_CODE
+FROM EMPLOYEE E, DEPARTMENT D
+WHERE E.DEPT_CODE = D.DEPT_ID;
+
+SELECT * FROM JOB;
+
+--사원명, 직급코드, 직급명 조회
+SELECT E.EMP_NAME,E.JOB_CODE, J.JOB_NAME
+FROM EMPLOYEE E JOIN JOB J ON E.JOB_CODE = J.JOB_CODE;
+
+
+
